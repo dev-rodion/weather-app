78fe392b3bf117a1cb6233b2986641191abe7f47
dev-rodion <gudkov.rodion.21@gmail.com>
2021-12-14 14:20:53 +0300

    Added styles and the background image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.scss";
 import weatherImg from "./images/png/jdwi-cloudy-day.png";
+import mountains from "./images/jpg/mountains.jpg";
 
 // Icons
 import { FiWind } from "react-icons/fi";
@@ -11,22 +12,24 @@ import { FiCloudSnow } from "react-icons/fi";
 import { FiCloud } from "react-icons/fi";
 import { FiDroplet } from "react-icons/fi";
 
-
+import { HiSearch } from "react-icons/hi";
+import { HiOutlineLocationMarker } from "react-icons/hi";
 
 function InfoItem({ icon, value }) {
   return (
-    <div className="item info__item">
+    <div className="item item_info">
       {icon}
       <p className="item__text">{value}</p>
     </div>
   );
 }
 
-function ForecastItem({ day, icon, max, min }) {
+function ForecastItem({ day, Icon, max, min }) {
   return (
-    <div className="item forecast__item">
-      <h6 className="item__title">{day}</h6>
-      {icon}
+    <div className="item item_forecast">
+      <h5 className="item__title">{day}</h5>
+      {/* <Icon className="item"/> */}
+      <Icon />
       <p className="item__text">
         {max}° <span>{min}°</span>
       </p>
@@ -36,23 +39,23 @@ function ForecastItem({ day, icon, max, min }) {
 
 function App() {
   return (
-    <div className="app">
+    <div className="app" style={{background: `url(${mountains})`}}>
       <div className="weather">
         <div className="weather__inner">
-          <form className="city">
-            <div className="city__icon">Marker Icon</div>
-            <input className="city__input" type="text" value="Sydney" />
-            <button type="submit">Find</button>
-          </form>
-
           <div className="info">
+            <form className="city">
+              <HiOutlineLocationMarker className="city__icon" />
+              <input className="city__input" type="text" value="Sydney" />
+              <button className="city__btn" type="submit"><HiSearch className="city__icon" /></button>
+            </form>
+          
             <div className="info__img">
               <img src={weatherImg} alt="Weather Icon" />
             </div>
 
-            <div className="info__temp">
-              <div className="info__temp">23°</div>
-              <div className="info__weather">Partly Cloudy</div>
+            <div className="info__main">
+              <h1 className="info__temp">23°</h1>
+              <h2 className="info__weather">Partly Cloudy</h2>
             </div>
 
             <div className="info__details">
@@ -63,10 +66,10 @@ function App() {
           </div>
 
           <div className="forecast">
-            <ForecastItem day="Tue" icon={<FiSun />} max="24" min="18" />
-            <ForecastItem day="Wed" icon={<FiCloudDrizzle />} max="23" min="19" />
-            <ForecastItem day="Thu" icon={<FiCloudLightning />} max="26" min="21" />
-            <ForecastItem day="Fri" icon={<FiCloud />} max="27" min="22" />
+            <ForecastItem day="Tue" Icon={FiSun} max="24" min="18" />
+            <ForecastItem day="Wed" Icon={FiCloudDrizzle} max="23" min="19" />
+            <ForecastItem day="Thu" Icon={FiCloudLightning} max="26" min="21" />
+            <ForecastItem day="Fri" Icon={FiCloud} max="27" min="22" />
           </div>
         </div>
       </div>
