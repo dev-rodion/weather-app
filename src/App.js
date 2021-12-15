3423de0dcebfcdcccc70b2824695e5b6b42388fd
dev-rodion <gudkov.rodion.21@gmail.com>
2021-12-15 16:57:49 +0300

    Added Open Weather API and scripts to get data from API

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "./App.scss";
 import weatherImg from "./images/png/jdwi-cloudy-day.png";
 import mountains from "./images/jpg/mountains.jpg";
@@ -8,7 +9,7 @@ import { FiSun } from "react-icons/fi";
 import { FiCloudDrizzle } from "react-icons/fi";
 import { FiCloudLightning } from "react-icons/fi";
 import { FiCloudRain } from "react-icons/fi";
-import { FiCloudSnow } from "react-icons/fi";
+// import { FiCloudSnow } from "react-icons/fi";
 import { FiCloud } from "react-icons/fi";
 import { FiDroplet } from "react-icons/fi";
 
@@ -38,30 +39,61 @@ function ForecastItem({ day, Icon, max, min }) {
 }
 
 function App() {
+  const [data, setData] = useState({});
+
+  const [city, setCity] = useState("Odesa");
+  const [coord, setCoord] = useState({ lat: 0, lon: 0 });
+
+  const apiKey = "79575682aba862a6a487f61e3520e0e4";
+  const apiCall1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const apiCall2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
+
+  useEffect(async () => {
+    await fetch(apiCall1)
+      .then((res) => res.json())
+      .then((data) => {
+        setCoord({ lat: data.coord.lat, lon: data.coord.lon });
+      });
+
+    await fetch(apiCall2)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+
   return (
-    <div className="app" style={{background: `url(${mountains})`}}>
+    <div className="app" style={{ background: `url(${mountains})` }}>
       <div className="weather">
         <div className="weather__inner">
           <div className="info">
             <form className="city">
               <HiOutlineLocationMarker className="city__icon" />
-              <input className="city__input" type="text" value="Sydney" />
-              <button className="city__btn" type="submit"><HiSearch className="city__icon" /></button>
+              <input
+                className="city__input"
+                type="text"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+              <button className="city__btn" type="submit">
+                <HiSearch className="city__icon" />
+              </button>
             </form>
-          
+
             <div className="info__img">
-              <img src={weatherImg} alt="Weather Icon" />
+              <img src={weatherImg} alt="" />
             </div>
 
             <div className="info__main">
-              <h1 className="info__temp">23°</h1>
-              <h2 className="info__weather">Partly Cloudy</h2>
+              <h1 className="info__temp">30°</h1>
+              <h2 className="info__weather">Sunny</h2>
             </div>
 
             <div className="info__details">
-              <InfoItem icon={<FiDroplet />} value="65%" />
-              <InfoItem icon={<FiCloudRain />} value="10%" />
-              <InfoItem icon={<FiWind />} value="23 km/h" />
+              <InfoItem icon={<FiDroplet />} value={`60%`} />
+              <InfoItem icon={<FiCloudRain />} value={`10%`} />
+              <InfoItem icon={<FiWind />} value={`25 km/h`} />
             </div>
           </div>
 
